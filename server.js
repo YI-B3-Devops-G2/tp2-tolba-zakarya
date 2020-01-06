2563c09c6842b191215a45a3f57942f98c8f5a4f
Zakarya Tolba <zakarya.tolba@ynov.com>
2020-01-06 10:13:04 +0100

    Fixed GET /status of the APi

--- server.js
+++ server.js
@@ -1,18 +1,49 @@
 'use strict';
 
 const express = require('express');
+const { Client } = require('pg');
+const redis = require('redis')
 
 // Constants
 const PORT = 8080;
-const HOST = '0.0.0.0';
+
+// PAS LES MEMES INFOS QUE DANS LE DOCKER-COMPOSE
+const pg = new Client({
+  user: "root",
+  password: "admin01",
+  host: "postgres_tp",
+  database: "test_tp_devops",
+  port: 5432,
+});
+const redisClient = redis.createClient({host: 'devops_tp_nico'});
 
 // App
 const app = express();
+
 app.get('/', (req, res) => {
-    res.json('Hello world\n');
+  res.send( { "message": "Hello World !" });
 });
-app.get('/status', (req, res) => {
-//
+
+app.get('/status', async (req, res) => {
+  const postgresQuery = 'SELECT date_trunc(\'second\', current_timestamp - pg_postmaster_start_time()) as uptime;'
+  const result = await pg.query(postgresQuery);
+  const uptime = result.rows[0].uptime;
+  const uptimeString = () => {
+    let time = '';
+
+    time += uptime.hours ? `${uptime.hours}h ` : '';
+    time += uptime.minutes ? `${uptime.minutes}m ` : '';
+    time += uptime.seconds ? `${uptime.seconds}s` : '';
+
+    return time
+  };
+
+  res.json({
+    status: 'ok',
+    postgresUptime: uptimeString(),
+    redisConnectedClients: Number(redisClient.server_info.connected_clients)
+  })
 });
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+pg.connect();
+app.listen(PORT);
+console.log('le serveur est lancer au port'+ PORT);
